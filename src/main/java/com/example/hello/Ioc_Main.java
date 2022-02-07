7a7fd56fcbd9c11ea2d9636e15cb6fe4ec743a41
HeLLo2 <popt0@naver.com>
2022-02-08 00:22:56 +0900

    How to use the DI

--- src/main/java/com/example/hello/Ioc_Main.java
+++ src/main/java/com/example/hello/Ioc_Main.java
@@ -8,8 +8,22 @@ public class Ioc_Main {
         String url = "www.naver.com/books/it?page=10&size=20&name=spring-boot";
 
         // Base 64 encoding
+//        IEncoder encoder = new Base64Encoder();
+//        System.out.println(encoder.encode(url));
 
+        // 객체가 직접 생성해서 가지고 있기.
+//        Encoder encoder = new Encoder();
 
-        System.out.println();
+        // DI (의존성 주입)
+        Encoder encoder = new Encoder(new Base64Encoder()); // 골라서 쓰세요!
+        String result = encoder.encode(url);        
+        System.out.println(result);
+    // It looks like subStream of file input output.
+        
+        
+        // Url encoding
+//        IEncoder urlEncoder = new UrlEncoder();
+//        System.out.println(urlEncoder.encode(url));
+
     }
 }
