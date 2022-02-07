35ffe43cf241c56f52e314cf10c00196f49f0298
HeLLo2 <popt0@naver.com>
2022-02-07 23:24:08 +0900

    How to use the Object Mapper

---
+++ src/main/java/com/example/hello/Ioc_Main.java
@@ -0,0 +1,15 @@
+package com.example.hello;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ioc_Main {
+    public static void main(String[] args) {
+        String url = "www.naver.com/books/it?page=10&size=20&name=spring-boot";
+
+        // Base 64 encoding
+
+
+        System.out.println();
+    }
+}
